e69f8c9e197004c30358681c9ce10bb8f90b71c4
Ragav-2002 <sripalasettyraghavendra2002@gmail.com>
2024-03-09 19:26:57 +0530

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+export const productData = [{id: 1, name: 'iphoneX', category: 'mobiles', price: 60000, stockQuantity: 15},
+{id: 2, name: 'iphone11', category: 'mobiles', price: 70000, stockQuantity: 20}, {id: 3, name: 'lenovo ideapad', category: 'laptops', price: 50000, stockQuantity: 10},
+{id: 4, name: 'hp spectre', category: 'laptops', price: 65000, stockQuantity: 8}, {id: 5, name: 'mi dual drivers', category: 'headphones', price: 800, stockQuantity: 25},
+{id: 6, name: 'boat rockers 550', category: 'headphones', price: 1200, stockQuantity: 19}, {id: 7, name: 'jojo volume 7', category: 'books', price: 900, stockQuantity: 5}]
+
+export const orderData = [{id: 1, name: 'iphoneX', category: 'mobiles', price: 60000, stockQuantity: 15}, {id: 5, name: 'mi dual drivers', category: 'headphones', price: 800, stockQuantity: 25},
+{id: 7, name: 'jojo volume 7', category: 'books', price: 900, stockQuantity: 5}]
